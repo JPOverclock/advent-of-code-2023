ea772611fee1bcd7f0362b6172734c99611a809f
Joao Goncalves <jpoverclock@gmail.com>
2023-12-20 22:50:57 +0000

    Day 17 - Part 2

--- src/main/java/com/jpoverclock/aoc/Day17.java
+++ src/main/java/com/jpoverclock/aoc/Day17.java
@@ -65,6 +65,70 @@ public class Day17 {
 
             return paths;
         }
+
+        public List<Path> nextUltra(int[][] grid, int height, int width) {
+            List<Path> paths = new LinkedList<>();
+
+            if (l < 4) {
+                // Must keep going
+                int nextI = i + dI;
+                int nextJ = j + dJ;
+
+                if (nextI >= 0 && nextJ >= 0 && nextI < height && nextJ < width) {
+                    paths.add(new Path(nextI, nextJ, dI, dJ, w + grid[nextI][nextJ], l + 1, Stream.concat(visited.stream(), Stream.of(this)).toList()));
+                }
+
+            } else if (l < 10) {
+                // Can keep going...
+                int nextI = i + dI;
+                int nextJ = j + dJ;
+
+                if (nextI >= 0 && nextJ >= 0 && nextI < height && nextJ < width) {
+                    paths.add(new Path(nextI, nextJ, dI, dJ, w + grid[nextI][nextJ], l + 1, Stream.concat(visited.stream(), Stream.of(this)).toList()));
+                }
+
+                // ... or turn
+                int nextDJ = dI;
+                int nextDI = dJ;
+                nextI = i + nextDI;
+                nextJ = j + nextDJ;
+
+                if (nextI >= 0 && nextJ >= 0 && nextI < height && nextJ < width) {
+                    paths.add(new Path(nextI, nextJ, nextDI, nextDJ, w + grid[nextI][nextJ], 1, Stream.concat(visited.stream(), Stream.of(this)).toList()));
+                }
+
+                nextDJ = -dI;
+                nextDI = -dJ;
+                nextI = i + nextDI;
+                nextJ = j + nextDJ;
+
+                if (nextI >= 0 && nextJ >= 0 && nextI < height && nextJ < width) {
+                    paths.add(new Path(nextI, nextJ, nextDI, nextDJ, w + grid[nextI][nextJ], 1, Stream.concat(visited.stream(), Stream.of(this)).toList()));
+                }
+
+            } else {
+                // Must turn
+                int nextDJ = dI;
+                int nextDI = dJ;
+                int nextI = i + nextDI;
+                int nextJ = j + nextDJ;
+
+                if (nextI >= 0 && nextJ >= 0 && nextI < height && nextJ < width) {
+                    paths.add(new Path(nextI, nextJ, nextDI, nextDJ, w + grid[nextI][nextJ], 1, Stream.concat(visited.stream(), Stream.of(this)).toList()));
+                }
+
+                nextDJ = -dI;
+                nextDI = -dJ;
+                nextI = i + nextDI;
+                nextJ = j + nextDJ;
+
+                if (nextI >= 0 && nextJ >= 0 && nextI < height && nextJ < width) {
+                    paths.add(new Path(nextI, nextJ, nextDI, nextDJ, w + grid[nextI][nextJ], 1, Stream.concat(visited.stream(), Stream.of(this)).toList()));
+                }
+            }
+
+            return paths;
+        }
     }
 
     record PointDirection(int i, int j, int dI, int dJ, int l) { }
@@ -113,6 +177,43 @@ public class Day17 {
     }
 
     public static String part2(String input) {
-        return "";
+        int[][] grid = parseInput(input);
+        int height = grid.length;
+        int width = grid[0].length;
+
+        PriorityQueue<Path> queue = new PriorityQueue<>(width * height, Comparator.comparingInt(Path::w));
+        queue.add(new Path(0, 0, 1, 0, 0, 0, List.of()));
+        queue.add(new Path(0, 0, 0, 1, 0, 0, List.of()));
+
+        Map<PointDirection, Integer> min = new HashMap<>();
+
+        for (int l = 0; l < 4; l++) {
+            min.put(new PointDirection(0, 0, 1, 0, l), 0);
+            min.put(new PointDirection(0, 0, 0, 1, l), 0);
+            min.put(new PointDirection(0, 0, -1, 0, l), 0);
+            min.put(new PointDirection(0, 0, 0, -1, l), 0);
+        }
+
+
+        while (!queue.isEmpty()) {
+            Path current = queue.remove();
+
+            if (current.i == (height - 1) && current.j == (width - 1) && current.l >= 4) {
+                System.out.println("Found a solution " + current);
+                return Integer.toString(current.w);
+            }
+
+            List<Path> expanded = current.nextUltra(grid, height, width);
+
+            for (var next : expanded) {
+                PointDirection pointDirection = new PointDirection(next.i, next.j, next.dI, next.dJ, next.l);
+                if (next.w < min.getOrDefault(pointDirection, Integer.MAX_VALUE)) {
+                    min.put(pointDirection, next.w);
+                    queue.add(next);
+                }
+            }
+        }
+
+        return "0";
     }
 }
