86f4ed8d175dab72d0865b2c723474d60301f524
Joao Goncalves <jpoverclock@gmail.com>
2023-12-28 19:15:32 +0000

    Day 20 - Part 1

---
+++ src/main/java/com/jpoverclock/aoc/Day20.java
@@ -0,0 +1,207 @@
+package com.jpoverclock.aoc;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Day20 {
+
+    enum Pulse {
+        LOW,
+        HIGH
+    }
+
+    record PulseAction(String origin, String destination, Pulse pulse) { }
+
+    static abstract class Module {
+
+        private final String name;
+
+        private final List<String> dependents;
+
+        private final List<String> incoming;
+
+        public Module(String name, List<String> dependents) {
+            this.name = name;
+            this.dependents = dependents;
+            this.incoming = new ArrayList<>();
+        }
+
+        abstract List<PulseAction> pulse(String from, Pulse pulse);
+
+        void consolidate(Map<String, Module> moduleMap) {
+            for (var dependentName : dependents) {
+                if (moduleMap.containsKey(dependentName)) {
+                    moduleMap.get(dependentName).hasIncomingSignalFrom(this.name);
+                }
+            }
+        }
+
+        void hasIncomingSignalFrom(String name) {
+            incoming.add(name);
+        }
+
+        protected List<PulseAction> issuePulses(Pulse pulse) {
+            return dependents.stream().map(d -> new PulseAction(this.name, d, pulse)).toList();
+        }
+
+        public static Module valueOf(String value) {
+            String[] parts = value.split(" -> ");
+            char type = parts[0].charAt(0);
+            String name = parts[0].substring(1);
+            List<String> dependents = Arrays.stream(parts[1].replaceAll("\\s+", "").split(",")).toList();
+
+            return switch (type) {
+                case '%' -> new FlipFlop(name, dependents);
+                case '&' -> new Conjunction(name, dependents);
+                case 'b' -> new Broadcast(parts[0], dependents);
+                default -> throw new RuntimeException("Invalid type " + type);
+            };
+        }
+    }
+
+    static class FlipFlop extends Module {
+
+        private Pulse status = Pulse.LOW;
+
+        public FlipFlop(String name, List<String> dependents) {
+            super(name, dependents);
+        }
+
+        @Override
+        List<PulseAction> pulse(String from, Pulse pulse) {
+            if (pulse.equals(Pulse.LOW)) {
+                status = (status == Pulse.LOW) ? Pulse.HIGH : Pulse.LOW;
+                return issuePulses(status);
+            }
+
+            return List.of();
+        }
+    }
+
+    static class Conjunction extends Module {
+
+        private final Map<String, Pulse> pulses = new HashMap<>();
+
+        public Conjunction(String name, List<String> dependents) {
+            super(name, dependents);
+        }
+
+        @Override
+        void hasIncomingSignalFrom(String name) {
+            super.hasIncomingSignalFrom(name);
+            pulses.put(name, Pulse.LOW);
+        }
+
+        @Override
+        List<PulseAction> pulse(String from, Pulse pulse) {
+            // Implement conjunction
+            pulses.put(from, pulse);
+
+            if (pulses.values().stream().allMatch(p -> p.equals(Pulse.HIGH))) {
+                return issuePulses(Pulse.LOW);
+            } else {
+                return issuePulses(Pulse.HIGH);
+            }
+        }
+    }
+
+    static class Broadcast extends Module {
+
+        public Broadcast(String name, List<String> dependents) {
+            super(name, dependents);
+        }
+
+        @Override
+        List<PulseAction> pulse(String from, Pulse pulse) {
+            return issuePulses(pulse);
+        }
+    }
+
+
+
+    public static String part1(String input) {
+        List<Module> modules = Arrays.stream(input.split("\n")).map(Module::valueOf).toList();
+        Map<String, Module> moduleMap = new HashMap<>();
+
+        // Consolidate
+        for (var module : modules) {
+            moduleMap.put(module.name, module);
+        }
+
+        for (var module : modules) {
+            module.consolidate(moduleMap);
+        }
+
+        // Pulse the machine 1000 times
+        BigInteger low = new BigInteger("1000");
+        BigInteger high = BigInteger.ZERO;
+
+        for (int i = 0; i < 1000; i++) {
+
+            List<PulseAction> actions = new ArrayList<>(moduleMap.get("broadcaster").pulse("button", Pulse.LOW));
+
+            while (!actions.isEmpty()) {
+                int actionsToProcess = actions.size();
+                for (int j = 0; j < actionsToProcess; j++) {
+                    PulseAction action = actions.remove(0);
+
+                    //System.out.println(action.origin + " -" + (action.pulse.equals(Pulse.LOW) ? "low" : "high") + "-> " + action.destination);
+
+                    if (action.pulse.equals(Pulse.LOW)) {
+                        low = low.add(BigInteger.ONE);
+                    } else {
+                        high = high.add(BigInteger.ONE);
+                    }
+
+                    Module module = moduleMap.get(action.destination);
+
+                    if (module != null) {
+                        actions.addAll(module.pulse(action.origin, action.pulse));
+                    }
+                }
+            }
+        }
+
+        return low.multiply(high).toString();
+    }
+
+    public static String part2(String input) {
+        List<Module> modules = Arrays.stream(input.split("\n")).map(Module::valueOf).toList();
+        Map<String, Module> moduleMap = new HashMap<>();
+
+        // Consolidate
+        for (var module : modules) {
+            moduleMap.put(module.name, module);
+        }
+
+        for (var module : modules) {
+            module.consolidate(moduleMap);
+        }
+
+        for (long i = 0;; i++) {
+
+            if (i % 100000 == 0) {
+                System.out.println(i);
+            }
+
+            List<PulseAction> actions = new ArrayList<>(moduleMap.get("broadcaster").pulse("button", Pulse.LOW));
+
+            while (!actions.isEmpty()) {
+                int actionsToProcess = actions.size();
+                for (int j = 0; j < actionsToProcess; j++) {
+                    PulseAction action = actions.remove(0);
+
+                    if (action.destination.equals("rx") && action.pulse.equals(Pulse.LOW)) {
+                        return Long.toString(i + 1);
+                    }
+
+                    Module module = moduleMap.get(action.destination);
+
+                    if (module != null) {
+                        actions.addAll(module.pulse(action.origin, action.pulse));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/com/jpoverclock/aoc/Day20Test.java
@@ -0,0 +1,100 @@
+package com.jpoverclock.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day20Test {
+
+    private static final String EXAMPLE_1 = """
+            broadcaster -> a, b, c
+            %a -> b
+            %b -> c
+            %c -> inv
+            &inv -> a
+            """;
+
+    private static final String EXAMPLE_2 = """
+            broadcaster -> a
+            %a -> inv, con
+            &inv -> b
+            %b -> con
+            &con -> output
+            """;
+
+    private static final String MY_INPUT = """
+            %jx -> rt, rs
+            &cc -> cd, fc, qr, nl, gk, zr
+            %qs -> cl, rs
+            %zr -> cq
+            %mx -> nr, pm
+            %mj -> qr, cc
+            %cj -> cc, nt
+            %jv -> sp
+            %dj -> bd, zc
+            %kt -> lt
+            broadcaster -> gz, xg, cd, sg
+            &dn -> rx
+            %br -> nf, bd
+            %cd -> cc, nl
+            %zc -> jq, bd
+            %xg -> cf, pm
+            %nz -> gm, bd
+            &dd -> dn
+            %nb -> sl
+            &pm -> kt, xg, xp, jv, sp
+            &fh -> dn
+            %rt -> qq
+            %qq -> rs, hd
+            %hd -> qs, rs
+            &xp -> dn
+            %pj -> cc, mj
+            %gz -> bd, kb
+            %zd -> jv, pm
+            %cq -> cj, cc
+            %qr -> gk
+            %ng -> jk, bd
+            %kb -> bd, sv
+            %cl -> zx, rs
+            %gj -> zd, pm
+            %sl -> kx
+            %sv -> br
+            %nf -> bd, nz
+            %zx -> rs
+            %nt -> mn, cc
+            %rh -> nb, rs
+            %gk -> ln
+            &bd -> gm, gz, fh, sv
+            %jq -> ng, bd
+            %sp -> pc
+            %sg -> rs, rh
+            %kx -> jx
+            &fc -> dn
+            %cf -> gj, pm
+            %pc -> kt, pm
+            %jk -> bd
+            %vf -> pm
+            &rs -> sg, dd, sl, kx, nb, rt
+            %nr -> vf, pm
+            %ln -> zr, cc
+            %lt -> pm, mx
+            %gm -> dj
+            %nl -> pj
+            %mn -> cc
+            """;
+
+    @Test
+    void part1Example1() {
+        assertEquals("32000000", Day20.part1(EXAMPLE_1));
+    }
+
+    @Test
+    void part1Example2() {
+        assertEquals("11687500", Day20.part1(EXAMPLE_2));
+    }
+
+    @Test
+    void part2() {
+        assertEquals("?", Day20.part2(MY_INPUT));
+    }
+}
